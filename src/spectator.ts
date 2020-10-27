ae41580af0a328456064b8459284a19aa7ea1c96
Fredrik Lange <fredrik.lange@bisnode.com>
2020-10-27 15:08:10 +0100

    Spectator can now execute federated query

--- src/spectator.ts
+++ src/spectator.ts
@@ -5,15 +5,17 @@ import {buildSchema, GraphQLSchema, print} from 'graphql';
 import {stitchSchemas, SubschemaConfig} from 'graphql-tools';
 import {ComEvents, ComManager} from "./transport/ComManager";
 import {UDPLink} from "./transport/Transport";
+import {ComponentRoles} from "./manager/app";
 
 const expr = express();
 
-const grapp = new ComManager({
+const comManager = new ComManager({
     appName: 'schemaSpectator',
-    link: new UDPLink(41236)
+    link: new UDPLink(41236),
+    role: ComponentRoles.SPECTATOR
 })
 
-grapp.connectWithPayload({})
+comManager.connectWithPayload({})
 
 // Construct a schema, using GraphQL schema language
 let schema = buildSchema(`
@@ -32,43 +34,49 @@ const root = {
 expr.listen(4000, () => {
     console.log('Running a GraphQL API server at http://localhost:4000/graphql');
 
-    expr.use('/graphql', graphQLDynamicSchema(schema));
+    expr.use('/graphql', graphQLHttpDynamicSchema(schema));
 });
 
-function graphQLDynamicSchema(initalSchema: GraphQLSchema): (req, res, next) => Promise<void> {
+interface StateRehydratePayload {
+    name: string
+    state: {
+        schemaSource: string
+    }
+}
+
+function graphQLHttpDynamicSchema(initalSchema: GraphQLSchema): (req, res, next) => Promise<void> {
     let _schema = initalSchema;
 
-    grapp.on(ComEvents.STATE_REHYDRATE, (event) => {
-        // TODO
+    comManager.on(ComEvents.STATE_REHYDRATE, (payload: StateRehydratePayload[]) => {
+        console.log('SPECTATOR REHYDRATES STATE', payload);
+        payload.forEach(p => {
+            _schema = stitch(_schema, createSubschema(p.state.schemaSource, p.name));
+            console.log('NEW SCHEMA SET');
+        })
     })
-    grapp.on(ComEvents.NEW_PEER, (event) => {
+    comManager.on(ComEvents.NEW_PEER, (event) => {
         console.log('SPECTATOR NOTICED A NEW COMPONENT');
         console.log('EVENT', event)
-
-        const newSchema = buildSchema(event.schemaSource);
-        const newSchema2 = {
-            schema: newSchema,
-            executor: async ({document, variables}) => {
-                const query = print(document);
-                let newVar = await grapp._exchange(event.name, {
-                    query, variables
-                });
-
-                console.log('Executor result', newVar)
-                return newVar
-            }
-
-        } as SubschemaConfig
-
-        let stitchedSchemas = stitchSchemas(
-            {subschemas: [_schema, newSchema2]}
-        )
-
-        _schema = stitchedSchemas;
+        _schema = stitch(_schema, createSubschema(event.schemaSource, event.name));
         console.log('NEW SCHEMA SET');
     });
 
     return (req, res, _) => graphqlHTTP({schema: _schema, rootValue: root, graphiql: true,})(req, res);
 }
 
+function createSubschema(source: string, remoteName: string): SubschemaConfig {
+    return {
+        schema: buildSchema(source),
+        executor: async ({document, variables}) =>
+            await comManager._exchange(remoteName, {
+                query: print(document), variables
+            })
 
+    } as SubschemaConfig
+}
+
+function stitch(...schemas): GraphQLSchema {
+    return stitchSchemas(
+        {subschemas: schemas}
+    )
+}
