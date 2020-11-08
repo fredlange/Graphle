b2e152304714d60e8cc1dbfa7e450327170523cf
Fredrik Lange <fredrik.lange@bisnode.com>
2020-11-08 11:08:20 +0100

    Orator now pings

--- src/clustering/orator/Orator.ts
+++ src/clustering/orator/Orator.ts
@@ -1,6 +1,8 @@
 import {ClusterManager} from "../ClusterManager";
 import {ComponentRoles} from "../../cluster-orator/app";
 import {ExchangeableLink} from "../link/ExchangeableLink";
+import {ComponentRegistry} from "../ComponentRegistry";
+import {IncomingMessage, LinkEvents} from "../link/ClusterLink";
 
 
 export class Orator extends ClusterManager {
@@ -11,21 +13,60 @@ export class Orator extends ClusterManager {
             role: ComponentRoles.PEER,
             appName: 'Orator',
             link: new ExchangeableLink({
-                serverPort: 1234
-            })
+                serverPort: 1234,
+                linkPort: 41236
+            }),
+            componentRegistry: new ComponentRegistry('Orator')
         })
 
-        setInterval(this.pingPeers, 5000)
+        setInterval(() => this.pingPeers(), 5000)
+
+
+        this.on('message', ((msg, rinfo) => {
+            const _msg = new IncomingMessage(msg, rinfo)
+
+
+
+            // switch (_msg.type) {
+            //     default: {
+            //         const component = _msg.sender.port
+            //         const payload = _msg.payload
+            //         let peersOfPeer = this.peers.getPeersOfComponent(component);
+            //
+            //         console.log('Peers of peer', peersOfPeer)
+            //
+            //         // Notify all peers of a new component
+            //         if (component.role == ComponentRoles.PEER) {
+            //             peersOfPeer.forEach(p => {
+            //                 this._send(p.port, {
+            //                     ...{component},
+            //                     ...payload
+            //                 })
+            //             })
+            //         }
+            //
+            //         // Return peers to the new component
+            //         let actualPeersOfPeer = peersOfPeer
+            //             .filter(p => p.role == ComponentRoles.PEER);
+            //         this.sendMessage(actualPeersOfPeer, component)
+            //
+            //         // Push new component intro componentRegistry
+            //         this.peers.pushOnNewComponent(component)
+            //     }
+            //
+            // }
+
+        }))
+
 
     }
 
     pingPeers() {
-        super.peers.getAllComponents().forEach(p => {
-            super._exchange(p.name, {})
-                .catch(e => {
-                    console.log('Need to remove peer', e)
-                })
-        })
+        console.log('Peers', this.peers)
+        this.peers
+            .getAllComponents()
+            .forEach(p => this._exchange(p.name, {}, LinkEvents.PING)
+                .catch(e => console.log('Need to remove peer', e)))
     }
 
 }
